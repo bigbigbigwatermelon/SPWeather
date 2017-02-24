32b58547b6fadbb5c4a27d02d7b99d43451b1e6a
fdccdffhb <coder.fdc@gmail.com>
2017-02-25 00:24:51 +0800

    [依赖库配置] 配置okhttp

--- app/src/main/java/team/gakkidaisuki/spweather/MainActivity.java
+++ app/src/main/java/team/gakkidaisuki/spweather/MainActivity.java
@@ -1,13 +1,44 @@
 package team.gakkidaisuki.spweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Log.d(TAG, "handleMessage: "+ MainActivity.this.run("https://api.heweather.com/v5/forecast?key=515562c1513741579ff9278765821dc7&city=%E5%8C%97%E4%BA%AC"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                super.run();
+            }
+        }.start();
     }
+
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
 }
